1d4c7edac65ef1b634cc4a79b64e910b0dcf1f79
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-06-01 19:41:14 +0100

    double speed of slow clock

--- Slow_Clock.vhd
+++ Slow_Clock.vhd
@@ -10,19 +10,19 @@ entity Slow_Clock is
 end Slow_Clock;
 
 architecture Behavioral of Slow_Clock is
-    signal prescaler : unsigned(26 downto 0) := (others => '0');
+    signal prescaler : unsigned(25 downto 0) := (others => '0');
     signal prev_bit  : std_logic := '0';
 begin
     process(clk)
     begin
         if rising_edge(clk) then
             prescaler <= prescaler + 1;
-				if (prev_bit = '0') and (prescaler(26) = '1') then
+				if (prev_bit = '0') and (prescaler(25) = '1') then
 					slow_clock <= '1';
 				else
 					slow_clock <= '0';
 				end if;
-            prev_bit <= prescaler(26);
+            prev_bit <= prescaler(25);
         end if;
     end process;
 end Behavioral;
