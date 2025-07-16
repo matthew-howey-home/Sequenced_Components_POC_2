874d6442bed7da86183a774a10d3c5aa47f42a7c
Matthew Howey <matthew.howey@yahoo.co.uk>
2025-07-16 21:10:03 +0100

    use write and read enable on register

--- Register.vhd
+++ Register.vhd
@@ -4,10 +4,11 @@ use IEEE.NUMERIC_STD.ALL;
 
 entity Register_with_enable is
     Port (
-        clk   		: in  std_logic;
-        enable    : in  std_logic;
-        data_in   : in  std_logic_vector(7 downto 0);
-        data_out  : out std_logic_vector(7 downto 0)
+        clk   				: in  std_logic;
+        write_enable    : in  std_logic;
+		  output_enable	: in 	std_logic;
+        data_in   		: in  std_logic_vector(7 downto 0);
+        data_out  		: out std_logic_vector(7 downto 0)
     );
 end Register_with_enable;
 
@@ -17,11 +18,11 @@ begin
     process(clk)
     begin
         if rising_edge(clk) then
-            if enable = '1' then
+            if write_enable = '1' then
                 register_internal <= data_in;
             end if;
         end if;
     end process;
 
-    data_out <= register_internal;
+    data_out <= register_internal when output_enable = '1' else (others => 'Z');
 end Behavioral;
